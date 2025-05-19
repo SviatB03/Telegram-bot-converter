0e929f2d2b1bb795af1d02f6ce6488010f31b872
SviatB03 <svatoslavbodnaruk@gmail.com>
2025-05-19 11:06:49 +0300

    Add files via upload

---
+++ .venv/lang.py
@@ -0,0 +1,62 @@
+MESSAGES = {
+    'language_choose': {
+        'uk': 'Оберіть мову 🌐',
+        'en': 'Choose a language 🌐',
+    },
+    'start': {
+        'uk': 'Привіт! Введіть суму для конвертації 💰',
+        'en': 'Hello! Enter the amount to convert 💰',
+    },
+    'invalid_amount': {
+        'uk': 'Невірний формат. Введіть число 🔢',
+        'en': 'Invalid format. Please enter a number 🔢',
+    },
+    'choose_pair': {
+        'uk': 'Оберіть пару валют 🔄',
+        'en': 'Choose currency pair 🔄',
+    },
+    'amount_too_small': {
+        'uk': 'Число повинно бути більше нуля. Спробуйте ще раз ⛔',
+        'en': 'Amount must be greater than zero. Try again ⛔',
+    },
+    'result': {
+        'uk': 'Отримуємо: {result} 💱',
+        'en': 'You get: {result} 💱',
+    },
+    'invalid_currency': {
+        'uk': 'Неправильна пара валют. Спробуйте ще раз 💱',
+        'en': 'Invalid currency pair. Try again 💱',
+    },
+    'enter_custom': {
+        'uk': 'Введіть свою валютну пару у форматі USD/UAH 🧾',
+        'en': 'Enter your currency pair in format USD/UAH 🧾',
+    },
+    'btn_convert': {
+        'uk': '💱 Конвертувати валюту',
+        'en': '💱 Convert Currency'
+    },
+    'btn_history': {
+        'uk': '📊 Історія конвертацій',
+        'en': '📊 Conversion History'
+    },
+    'btn_help': {
+        'uk': '🆘 Допомога',
+        'en': '🆘 Help'
+    },
+    'btn_about': {
+        'uk': 'ℹ️ Про бота',
+        'en': 'ℹ️ About'
+    },
+    'help': {
+        'uk': 'Надішліть суму для конвертації, оберіть пару валют або введіть свою. Ви можете також переглянути історію 📊.',
+        'en': 'Send an amount to convert, choose a currency pair, or enter your own. You can also view history 📊.'
+    },
+    'about': {
+        'uk': 'Цей бот конвертує валюти за курсами Exchangerate-API. Створено з ❤️',
+        'en': 'This bot converts currencies using Exchangerate-API rates. Made with ❤️'
+    },
+    'history_empty': {
+        'uk': 'Історія пуста 📭',
+        'en': 'History is empty 📭'
+    },
+}
